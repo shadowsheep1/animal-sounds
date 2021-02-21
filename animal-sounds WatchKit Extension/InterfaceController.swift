9a5ca2c9aa590490d6374e5cc2787c768a59c860
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2021-02-21 10:54:40 +0100

    initial commit

---
+++ animal-sounds WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  animal-sounds WatchKit Extension
+//
+//  Created by Fabio Bombardi on 21/02/21.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
